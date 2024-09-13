662c8edac21f45f7d08d1df69ea50fa484646ad6
DKhorkov <dkhorkov.dev@gmail.com>
2024-09-14 01:02:11 +0300

    little fixes

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,8 +5,8 @@ import "github.com/DKhorkov/hmtm-bff/pkg/loadenv"
 func GetConfig() *Config {
 	return &Config{
 		HTTP: HTTPConfigs{
-			Host:              loadenv.GetEnv("GRAPHQL_HOST", "0.0.0.0"),
-			Port:              loadenv.GetEnvAsInt("GRAPHQL_PORT", 8080),
+			Host:              loadenv.GetEnv("HOST", "0.0.0.0"),
+			Port:              loadenv.GetEnvAsInt("PORT", 8080),
 			ReadHeaderTimeout: loadenv.GetEnvAsInt("HTTP_READ_HEADER_TIMEOUT", 1),
 		},
 	}
